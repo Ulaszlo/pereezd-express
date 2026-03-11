604721af47758f5a422d8ca87b64c509daf2d849
Ulaszlo <cool.tsuranov@inbox.ru>
2026-03-11 21:45:55 +0300

    add FSD

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import Image from "next/image";
 import styles from "./page.module.css";
+import { Header } from "@/widgets/header";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-
+        <Header />
       </main>
     </div>
   );
--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -1,9 +1,22 @@
 import styles from './Header.module.scss';
+import Link from "next/link";
 
 export const Header = () => {
   return (
     <header className={styles.header}>
+      <div className={styles.container}>
+        <Link href="/">
+          Logo
+        </Link>
 
+        <Link href="/">
+          +7 (4012) 99-44-99
+        </Link>
+
+        <button>
+          Menu
+        </button>
+      </div>
     </header>
   );
 };
